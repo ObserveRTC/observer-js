988fb2f3a333744d070228618ef4c10987722527
pallab-gain <pallab.gain@gmail.com>
2020-05-30 19:58:07 +0300

    Remove loglevel from the dependencies

--- src/wobserver.logger/index.ts
+++ src/wobserver.logger/index.ts
@@ -1,20 +1,24 @@
-import * as logger from 'loglevel'
-import * as prefix from 'loglevel-plugin-prefix'
+import * as Logger from 'loglevel'
 
-prefix.reg(logger)
-logger.enableAll()
-
-prefix.apply(logger, {
-    template: '[%t] %l (%n) static text:',
-    levelFormatter(level: string) {
-        return level.toUpperCase()
-    },
-    nameFormatter(name) {
-        return name || 'global'
-    },
-    timestampFormatter(date) {
-        return date.toISOString()
-    },
-})
-
+function initLogger(name: string, prefix: any) {
+    const result = Logger.getLogger(name)
+    result.enableAll()
+    if (!prefix) return result
+    // @ts-ignore
+    result.prefix = prefix
+    const original = {
+        debug: result.debug.bind(result),
+        error: result.error.bind(result),
+        info: result.info.bind(result),
+        trace: result.trace.bind(result),
+        warn: result.warn.bind(result),
+    }
+    result.trace = (...args) => original.trace((typeof prefix === 'function' ? prefix() : prefix), ...args)
+    result.debug = (...args) => original.debug((typeof prefix === 'function' ? prefix() : prefix), ...args)
+    result.info = (...args) => original.info((typeof prefix === 'function' ? prefix() : prefix), ...args)
+    result.warn = (...args) => original.warn((typeof prefix === 'function' ? prefix() : prefix), ...args)
+    result.error = (...args) => original.error((typeof prefix === 'function' ? prefix() : prefix), ...args)
+    return result
+}
+const logger = initLogger('webextrapp', () => `${new Date().toISOString()}`)
 export default logger
