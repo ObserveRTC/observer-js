9aae92da99d358e54b50a35397e43c61c9eb8528
pallab-gain <pallab.gain@gmail.com>
2020-05-30 16:27:07 +0300

    Wobset server address is parameterised

--- src/wobserver.plugins/stats.sender/index.ts
+++ src/wobserver.plugins/stats.sender/index.ts
@@ -3,13 +3,14 @@ import WobserverPC from '../../wobserver.core/pc.manager/wobserver.pc'
 import logger from '../../wobserver.logger'
 import { WobserverPlugin } from '../index'
 
-const webSocketAddress = 'ws://localhost:8080/ws/demo/86ed98c6-b001-48bb-b31e-da638b979c72'
-
 class StatsSender extends WobserverPlugin{
     private readonly webSocket!: WebSocket
-    constructor() {
+    constructor(serverAddress: string) {
         super()
-        // this.webSocket = new WebSocket(webSocketAddress)
+        if (!serverAddress) {
+            throw new Error('websocker server address is required')
+        }
+        this.webSocket = new WebSocket(serverAddress)
     }
 
     async execute(pc: WobserverPC): Promise<any> {
@@ -30,7 +31,7 @@ class StatsSender extends WobserverPlugin{
             return
         }
         logger.warn('sending message to server', payload)
-        // this.webSocket?.send(payload)
+        this.webSocket?.send(JSON.stringify(payload))
     }
 }
 
