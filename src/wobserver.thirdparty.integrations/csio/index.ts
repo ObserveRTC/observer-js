aff28fea793d8ce43810092b298d93dcbe4a8588
Karthik <karthik@Karthik-BRs-MacBook-Pro.local>
2020-08-18 19:57:26 +0300

    fixes in addnewfabric

--- src/wobserver.thirdparty.integrations/csio/index.ts
+++ src/wobserver.thirdparty.integrations/csio/index.ts
@@ -2,7 +2,7 @@ import { WobServerThridParty } from '../index'
 import Wobserver from '../../wobserver.core'
 import StatsParser from '../../wobserver.plugins/stats.parser'
 import StatsSender from '../../wobserver.plugins/stats.sender'
-
+const serverURL = 'wss://meet.cogint.ai:7879/ws/86ed98c6-b001-48bb-b31e-da638b979c72'
 class callstats extends WobServerThridParty {
     public readonly errors = {
         httpError: 'httpError',
@@ -105,50 +105,62 @@ class callstats extends WobServerThridParty {
     statsSender: any;
     wobserver: any;
 
-    public async initialize(appId: any, appSecret: any, userId: any, initCallback: any) {
-        console.log('********* callstats initialization ', appId, appSecret);
+    public initialize(appId: any, appSecret: any, userId: any, initCallback: any) {
+        console.log('******** callstats initialization ', appId, appSecret);
         this.statsParser = new StatsParser();
-        this.statsSender = new StatsSender('');
+        this.statsSender = new StatsSender(serverURL);
         this.wobserver = new Wobserver();
         this.wobserver.attachPlugin(this.statsParser)
         this.wobserver.attachPlugin(this.statsSender)
         this.wobserver.startWorker()
         if (initCallback) {
-            initCallback('SDK authentication successful.');
+            setTimeout(function(){ initCallback('success', 'SDK authentication successful.');}, 1000);
         }
         return {status: 'success'};
       }
     
-    public async addNewFabric(pc: any, remoteId: any, fabricUsage: any, conferenceId: any, fabricAttributes: any) {
+    public addNewFabric(pc: any, remoteId: any, fabricUsage: any, conferenceId: any, fabricAttributes: any) {
         console.log('********* callstats addNewFabric ', pc, remoteId);
+        try {
+            this.wobserver.addPC(pc);
+        } catch(e) {
+            console.log('******** addpc error', e);
+        }
+        return {status: 'success', message: 'success++'};
+    }
+    
+    public sendFabricEvent(pc: any, fabricEvent: any, conferenceId: any, eventData: any) {
+        console.log('********* callstats sendFabricEvent ');
         return {status: 'success'};
     }
     
-    public async sendFabricEvent(pc: any, fabricEvent: any, conferenceId: any, eventData: any) {
+    public sendCustomEvent(pc: any, conferenceId: any, eventList: any) {
+        console.log('********* callstats sendCustomEvent ');
         return {status: 'success'};
     }
     
-    public async sendCustomEvent(pc: any, conferenceId: any, eventList: any) {
+    public sendCustomStats(pc: any, conferenceId: any, stats: any) {
+        console.log('********* callstats sendCustomStats ');
         return {status: 'success'};
     }
     
-    public async sendCustomStats(pc: any, conferenceId: any, stats: any) {
+    public  sendUserFeedback(conferenceId: any, feedback: any, callback: any) {
+        console.log('********* callstats sendUserFeedback ');
         return {status: 'success'};
     }
     
-    public async  sendUserFeedback(conferenceId: any, feedback: any, callback: any) {
+    public associateMstWithUserID(pc: any, remoteId: any, conferenceId: any, ssrc: any, usage: any, videoTag: any) {
+        console.log('********* callstats associateMstWithUserID ');
         return {status: 'success'};
     }
     
-    public async associateMstWithUserID(pc: any, remoteId: any, conferenceId: any, ssrc: any, usage: any, videoTag: any) {
+    public reportError(pc: any, conferenceId: any, functionName: any, domError: any, localSDP: any, remoteSDP: any) {
+        console.log('********* callstats reportError ');
         return {status: 'success'};
     }
     
-    public async reportError(pc: any, conferenceId: any, functionName: any, domError: any, localSDP: any, remoteSDP: any) {
-        return {status: 'success'};
-    }
-    
-    public async  attachWifiStatsHandler(getWifiStatsMethod: any) {
+    public  attachWifiStatsHandler(getWifiStatsMethod: any) {
+        console.log('********* callstats attachWifiStatsHandler ');
         return {status: 'success'};
     }
 }
