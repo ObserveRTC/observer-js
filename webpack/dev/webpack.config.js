67c902b0b82c7976cc7eacf67e2f9489e6b8b309
pallab-gain <pallab.gain@gmail.com>
2020-10-04 18:25:45 +0300

    Add webpack config

---
+++ webpack/dev/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const libraryConfig = require('../../project.config/core')
+const {version} = require('../../package.json')
+
+module.exports = {
+    entry: {
+        'observer-lib': './build/default.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../../', 'dist', 'core'),
+        filename: "observer.js",
+        library: "ObserverRTC",
+        umdNamedDefine: true,
+        libraryExport: "default",
+        libraryTarget: "umd"
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                loader: 'ts-loader',
+            },
+        ],
+    },
+    optimization: {
+        minimize: false
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new webpack.DefinePlugin({
+            LIBRARY_VERSION: JSON.stringify(version),
+            DEBUG: JSON.stringify(libraryConfig.debug),
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true
+        })
+    ],
+};
