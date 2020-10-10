6923986ac819a2a89fee929e63cc62d604b68ec2
pallab-gain <pallab.gain@gmail.com>
2020-10-10 16:29:11 +0300

    Update library and publish new version

--- dist/observer.js
+++ dist/observer.js
@@ -306,7 +306,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const builder_1 = __importDefault(__webpack_require__(/*! ./observer.manager/builder */ "./build/observer.manager/builder/index.js"));
 const stats_parser_plugin_1 = __importDefault(__webpack_require__(/*! ./observer.plugins/public/stats.parser.plugin */ "./build/observer.plugins/public/stats.parser.plugin/index.js"));
 const websocket_sender_plugin_1 = __importDefault(__webpack_require__(/*! ./observer.plugins/public/websocket.sender.plugin */ "./build/observer.plugins/public/websocket.sender.plugin/index.js"));
-exports.default = { Builder: builder_1.default, StatsParser: stats_parser_plugin_1.default, StatsSender: websocket_sender_plugin_1.default };
+const parser_util_1 = __importDefault(__webpack_require__(/*! ./observer.utils/parser.util */ "./build/observer.utils/parser.util/index.js"));
+exports.default = { Builder: builder_1.default, StatsParser: stats_parser_plugin_1.default, StatsSender: websocket_sender_plugin_1.default, ParserUtil: parser_util_1.default };
 //# sourceMappingURL=index.js.map
 
 /***/ }),
@@ -514,7 +515,7 @@ class Observer {
         // @ts-ignore
         this.intervalWorker = new observer_interval_worker_1.default(parseInt(typeof POOLING_INTERVAL_MS === 'undefined' ? 1000 : POOLING_INTERVAL_MS, 10));
         // @ts-ignore
-        console.info('using library version', "0.2.6");
+        console.info('using library version', "0.2.7");
     }
     attachPlugin(plugin) {
         if (this.pluginList.find(item => item.id === plugin.id)) {
@@ -1056,6 +1057,42 @@ class BrowserUtil {
     }
 }
 exports.default = BrowserUtil;
+//# sourceMappingURL=index.js.map
+
+/***/ }),
+
+/***/ "./build/observer.utils/parser.util/index.js":
+/*!***************************************************!*\
+  !*** ./build/observer.utils/parser.util/index.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class ParserUtil {
+    static parseWsServerUrl(serverURL, serviceUUID, mediaUnitId, statsVersion) {
+        if (!serverURL) {
+            throw Error('server url is undefined');
+        }
+        if (!serviceUUID) {
+            throw Error('service UUID is undefined');
+        }
+        if (!mediaUnitId) {
+            throw Error('media unit id is undefined');
+        }
+        if (!statsVersion) {
+            throw Error('stats version is undefined');
+        }
+        serverURL = `${serverURL.replace(/\/$/, '')}`;
+        serviceUUID = `${serviceUUID.replace(/^\//, '')}`;
+        mediaUnitId = `${mediaUnitId.replace(/^\//, '')}`;
+        statsVersion = `${statsVersion.replace(/^\//, '')}`;
+        return `${serverURL}/${serviceUUID}/${mediaUnitId}/${statsVersion}/json`;
+    }
+}
+exports.default = ParserUtil;
 //# sourceMappingURL=index.js.map
 
 /***/ }),
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import ObserverBuilder from './observer.manager/builder'
 import StatsParser from './observer.plugins/public/stats.parser.plugin'
 import StatsSender from './observer.plugins/public/websocket.sender.plugin'
+import ParserUtil from './observer.utils/parser.util'
 
-export default { Builder: ObserverBuilder, StatsParser , StatsSender}
+export default { Builder: ObserverBuilder, StatsParser , StatsSender, ParserUtil}
