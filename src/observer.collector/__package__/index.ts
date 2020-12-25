6f0aca97032b837c14bf68dc04a232ee6cce1fa2
pallab-gain <pallab.gain@gmail.com>
2020-12-25 18:39:42 +0200

    Removed redundant code

--- src/observer.collector/__package__/index.ts
+++ src/observer.collector/__package__/index.ts
@@ -1,6 +1,4 @@
 import { Observer } from "../observer";
 import { Builder } from "../observer.builder";
 import { ObserverPC } from "../observer.peer";
-import { helloWorld } from '../../observer.processtor';
-
-export { Builder, ObserverPC, Observer, helloWorld };
+export { Builder, ObserverPC, Observer };
