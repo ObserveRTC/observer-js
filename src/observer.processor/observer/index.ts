fc5ec0aba53325f73871d909e7d917a9d9f86f63
pallab-gain <pallab.gain@gmail.com>
2020-12-26 23:01:05 +0200

    Removed redundant import

--- src/observer.processor/observer/index.ts
+++ src/observer.processor/observer/index.ts
@@ -53,6 +53,10 @@ class ObserverProcessor implements WorkerCallback {
     }
 }
 
-export {
-    ObserverProcessor
-}
+const observerProcessor = new ObserverProcessor()
+// Update worker scope
+observerProcessor.updateWorkerInstance(self)
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+onmessage = observerProcessor.messageHandler
+// Start cron task
+observerProcessor.startCronTask()
