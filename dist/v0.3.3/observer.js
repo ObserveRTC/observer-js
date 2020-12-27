670e0c0bd9af25211625c99d48a6206bb1bd926a
pallab-gain <pallab.gain@gmail.com>
2020-12-27 21:49:25 +0200

    new library version

--- dist/v0.3.3/observer.js
+++ dist/v0.3.3/observer.js
@@ -2107,7 +2107,7 @@
 	        // @ts-expect-error Will be injected in build time
 	        "0.3.3", 'from build date', 
 	        // @ts-expect-error Will be injected in build time
-	        "Sun, 27 Dec 2020 19:35:53 GMT");
+	        "Sun, 27 Dec 2020 19:49:03 GMT");
 	    }
 	    onError(_err) {
 	        // Pass
