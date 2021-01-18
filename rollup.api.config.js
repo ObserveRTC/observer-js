8d462f2476d55be35e97ab46043cae94a02f8612
pallab-gain <pallab.gain@gmail.com>
2021-01-18 13:01:36 +0200

    Update rollup build script

--- rollup.api.config.js
+++ rollup.api.config.js
@@ -7,24 +7,37 @@ const dtsPlugin = require('rollup-plugin-dts').default
 const licensePlugin = require('rollup-plugin-license')
 const { version, config } = require('./package.json')
 import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace';
+import replace from '@rollup/plugin-replace'
+
+
+const getLicense = () => {
+  const license = licensePlugin({
+    banner: {
+      content: {
+        file: path.join(__dirname, 'LICENSE.md'),
+      },
+    },
+  })
+  return license
+}
+const getWorkerURL = (isProduction = false) => {
+  const workerURL = isProduction ? `${config.workerLoadEndpoint}/${version}/observer.worker.min.js` : `${config.workerLoadEndpoint}/${version}/observer.worker.js`
+  return workerURL
+}
+
 
 const outputDirectory = `dist/v${version}`
 const outputDirectoryLatest = `dist/latest`
-const commonBanner = licensePlugin({
-  banner: {
-    content: {
-      file: path.join(__dirname, 'LICENSE.md'),
-    },
-  },
-})
 const buildDate = JSON.stringify(new Date().toUTCString())
 const buildVersion = JSON.stringify(version)
 const isProd = process.env.npm_lifecycle_event === 'build'
-const workerURL = JSON.stringify(isProd ? config.workerProdURL : config.workerDevURL)
+const workerURL = JSON.stringify(getWorkerURL(isProd))
+const commonTerser = terserPlugin(require('./terser.config.js'))
+
+const collectorRootFilePath = './src/observer.collector/__package__/index.ts'
+const workerRootFilePath = './src/observer.processor/__package__/index.ts'
 
 const commonInput = {
-  input: './src/observer.collector/__package__/index.ts',
   plugins: [
     nodeResolvePlugin({
       browser: true,
@@ -34,71 +47,66 @@ const commonInput = {
       declaration: false,
     }),
     commonjs(),
-    commonBanner,
+    getLicense(),
     replace({
       __buildDate__: buildDate,
       __buildVersion__: buildVersion,
       __workerUrl__: workerURL,
-      __isDebug__: JSON.stringify(isProd === false)
-    })
+      __isDebug__: JSON.stringify(isProd === false),
+    }),
   ],
 }
 
-const commonOutput = {
+const collectorLibraryOutput = {
   name: 'ObserverRTC',
   exports: 'named',
 }
 
-const commonTerser = terserPlugin(require('./terser.config.js'))
-
-const buildDev =  {
-  ...commonOutput,
-  file: `${outputDirectory}/observer.js`,
-  format: 'umd',
+const workerLibraryOutput = {
+  name: 'ObserverWorkerRTC',
+  exports: 'named',
 }
 
-const buildDevLatest =  {
-  ...buildDev,
-  file: `${outputDirectoryLatest}/observer.js`,
-}
+const buildLibrary = (isProduction = false, outputLatest = false, isWorker = false) => {
+  const libraryFileName = isProduction ? (isWorker ? 'observer.worker.min' : 'observer.min') : (isWorker ? 'observer.worker' : 'observer')
+  const libraryFileNameDefinition = 'observer.d'
+  const currentOutputDirectory = outputLatest === true ? outputDirectoryLatest : outputDirectory
+  const rootFilePath = isWorker ? workerRootFilePath : collectorRootFilePath
 
-const buildProduction = {
-  ...commonOutput,
-  file: `${outputDirectory}/observer.min.js`,
-  format: 'umd',
-  plugins: [commonTerser],
-}
+  const buildCollectorLibrary = {
+    ...(isWorker ? workerLibraryOutput : collectorLibraryOutput),
+    file: `${currentOutputDirectory}/${libraryFileName}.js`,
+    format: 'umd',
+    ...(isProduction && { plugins: [commonTerser] }),
+  }
 
-const buildProductionLatest = {
-  ...buildProduction,
-  file: `${outputDirectoryLatest}/observer.min.js`,
+  const buildConfig = []
+  buildConfig.push(
+    // Library building
+    {
+      ...commonInput,
+      input: rootFilePath,
+      output: [
+        buildCollectorLibrary,
+      ],
+    },
+  )
+  if (!isWorker) {
+    // TypeScript definition
+    buildConfig.push({
+      ...commonInput,
+      input: rootFilePath,
+      plugins: [dtsPlugin(), getLicense()],
+      output: {
+        file: `${outputDirectory}/${libraryFileNameDefinition}.ts`,
+        format: 'es',
+      },
+    })
+  }
+  return buildConfig
 }
 
 module.exports = [
-  // Browser bundles. They have all the dependencies included for convenience.
-  {
-    ...commonInput,
-    output: [
-      isProd ? buildProduction : buildDev,
-      isProd ? buildProductionLatest : buildDevLatest,
-    ],
-  },
-
-  // TypeScript definition
-  {
-    ...commonInput,
-    plugins: [dtsPlugin(), commonBanner],
-    output: {
-      file: `${outputDirectory}/observer.d.ts`,
-      format: 'es',
-    },
-  },
-  {
-    ...commonInput,
-    plugins: [dtsPlugin(), commonBanner],
-    output: {
-      file: `${outputDirectoryLatest}/observer.d.ts`,
-      format: 'es',
-    },
-  },
+  ...buildLibrary(isProd, false, false),
+  ...buildLibrary(isProd, false, true),
 ]
