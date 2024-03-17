d46c21371302c8c9b25a585a8df531cfa24bafe6
Balazs Kreith <kreith.balazs@gmail.com>
2024-03-17 11:37:50 +0200

    save

--- src/monitors/TurnUsageMonitor.ts
+++ src/monitors/TurnUsageMonitor.ts
@@ -4,6 +4,10 @@ import { ObservedICEEvents } from '../ObservedICE';
 
 export type TurnUsageMonitorEvents = {
 	close: [],
+	'newturnip': [string],
+	update: [TurnUsage],
+	addpeerconnection: [ObservedPeerConnection],
+	removepeerconnection: [ObservedPeerConnection],
 }
 
 export type TurnUsage = {
@@ -24,6 +28,7 @@ export declare interface TurnUsageMonitor {
 	on<U extends keyof TurnUsageMonitorEvents>(event: U, listener: (...args: TurnUsageMonitorEvents[U]) => void): this;
 	off<U extends keyof TurnUsageMonitorEvents>(event: U, listener: (...args: TurnUsageMonitorEvents[U]) => void): this;
 	once<U extends keyof TurnUsageMonitorEvents>(event: U, listener: (...args: TurnUsageMonitorEvents[U]) => void): this;
+	emit<U extends keyof TurnUsageMonitorEvents>(event: U, ...args: TurnUsageMonitorEvents[U]): boolean;
 }
 
 export class TurnUsageMonitor extends EventEmitter {
@@ -40,6 +45,7 @@ export class TurnUsageMonitor extends EventEmitter {
 		if (this._connections.has(peerConnection.peerConnectionId)) return;
 
 		const ice = peerConnection.ICE;
+
 		const onUpdate = () => {
 			const turnIp = ice?.selectedRemoteCandidate?.address;
 			
@@ -53,34 +59,44 @@ export class TurnUsageMonitor extends EventEmitter {
 					totalBytesSent: 0,
 					totalBytesReceived: 0,
 					totalPacketsSent: 0,
-					totalPacketsReceived: 0
+					totalPacketsReceived: 0,
 				};
 				this._turnUsage.set(turnIp, usage);
+
+				this.emit('newturnip', turnIp);
 			}
 			
 			usage.totalBytesSent += ice.deltaBytesSent;
 			usage.totalBytesReceived += ice.deltaBytesReceived;
 			usage.totalPacketsSent += ice.deltaPacketsSent;
 			usage.totalPacketsReceived += ice.deltaPacketsReceived;
+
+			this.emit('update', usage);
 		};
+
+		peerConnection.ICE.on('update', onUpdate);
 
 		this._connections.set(peerConnection.peerConnectionId, {
 			peerConnection,
 			onUpdate
 		});
 
-		peerConnection.ICE.on('update', onUpdate);
+		this.emit('addpeerconnection', peerConnection);
 	}
 
 	public removePeerConnection(peerConnection: ObservedPeerConnection) {
 		if (this.closed) return;
 		if (!this._connections.has(peerConnection.peerConnectionId)) return;
-		
+
 		const { onUpdate } = this._connections.get(peerConnection.peerConnectionId) ?? {};
 		
 		if (!onUpdate) return;
 
 		peerConnection.ICE.off('update', onUpdate);
+
+		this._connections.delete(peerConnection.peerConnectionId);
+
+		this.emit('removepeerconnection', peerConnection);
 	}
 
 	public get turnIps() {
